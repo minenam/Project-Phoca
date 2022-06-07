59af49516c908b3c07daa628fde052bba4eb639a
rnrn99 <giu0213@naver.com>
2022-06-07 19:21:56 +0900

    [ADD] dropzone setting

---
+++ front/components/upload/Dropzone.tsx
@@ -0,0 +1,36 @@
+import { useState, useEffect, useCallback } from "react";
+import { useDropzone, DropzoneProps } from "react-dropzone";
+
+interface dropzoneProps extends DropzoneProps {
+  files?: File[];
+  onDrop?: (acceptedFiles: File[]) => void;
+}
+
+const Upload = (props: dropzoneProps) => {
+  const [files, setFiles] = useState<File[]>([]); // 업로드할 파일
+
+  // drop handler
+  const onDrop = useCallback((acceptedFiles: File[]) => {
+    setFiles(
+      acceptedFiles.map((file) =>
+        Object.assign(file, {
+          preview: URL.createObjectURL(file),
+        }),
+      ),
+    );
+  }, []);
+  const { getRootProps, getInputProps } = useDropzone({ onDrop });
+
+  useEffect(() => {
+    console.log(files);
+  }, [files]);
+
+  return (
+    <div {...getRootProps()} style={{ border: "1px solid black" }}>
+      <input {...getInputProps()} />
+      <p>Drag drop some files here, or click to select files</p>
+    </div>
+  );
+};
+
+export default Upload;
---
+++ front/pages/upload/index.tsx
@@ -0,0 +1,12 @@
+import Upload from "../../components/upload/Dropzone";
+
+const UploadPage = () => {
+  return (
+    <div>
+      UploadPage
+      <Upload />
+    </div>
+  );
+};
+
+export default UploadPage;
