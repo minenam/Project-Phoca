b152b4eb33870b0b814e5092ffee558e2988525e
Changm-L <chang6585@naver.com>
2022-06-08 14:41:38 +0900

    [Add]: styletron init

--- front/pages/_app.tsx
+++ front/pages/_app.tsx
@@ -1,8 +1,14 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { Provider as StyletronProvider } from "styletron-react";
+import { styletron } from "../styletron";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <StyletronProvider value={styletron}>
+      <Component {...pageProps} />
+    </StyletronProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
