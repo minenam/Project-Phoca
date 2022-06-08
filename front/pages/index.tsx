6036cad3c9e9bcab8e4aca7bd2a51e4deafa7a71
Changm-L <chang6585@naver.com>
2022-06-08 14:41:18 +0900

    [Add]: NavBar Component working

--- front/pages/index.tsx
+++ front/pages/index.tsx
@@ -1,7 +1,13 @@
 import type { NextPage } from "next";
+import NavBar from "../components/NavBar";
 
 const Home: NextPage = () => {
-  return <div>MainPage</div>;
+  return (
+    <div>
+      <NavBar />
+      MainPage
+    </div>
+  );
 };
 
 export default Home;
