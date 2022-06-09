d3c6b8ad57a704bb87495e6a018a62e35a544f87
rnrn99 <giu0213@naver.com>
2022-06-09 14:32:14 +0900

    [Fix] border 삭제

--- front/components/word/Note.style.ts
+++ front/components/word/Note.style.ts
@@ -3,7 +3,6 @@ import { styled } from "styletron-react";
 export const NoteContainer = styled("div", (props) => ({
   width: "100%",
   height: "100vh",
-  border: "1px solid red",
   display: "flex",
   alignItems: "center",
 }));
