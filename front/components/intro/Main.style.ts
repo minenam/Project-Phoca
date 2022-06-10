6e042015159c5abc8d7ca1031dbfb5b6d66d1625
changMin <Dlatlqjsgh7862!>
2022-06-10 18:53:24 +0900

    [Add] Button hover css & background image

--- front/components/intro/Main.style.ts
+++ front/components/intro/Main.style.ts
@@ -1,6 +1,7 @@
 import { styled } from "styletron-react";
 
 export const MainButton = styled("div", (props: { $guide?: Boolean }) => ({
+  position: "relative",
   display: "flex",
   justifyContent: "center",
   alignItems: "center",
@@ -11,16 +12,41 @@ export const MainButton = styled("div", (props: { $guide?: Boolean }) => ({
   fontFamily: "Nanum Gothic Coding, monospace",
   fontWeight: "bold",
   fontSize: "1.5rem",
+
   height: props.$guide ? "100%" : "300px",
   gridRowStart: props.$guide && 1,
   gridRowEnd: props.$guide && 3,
   backgroundColor: props.$guide && "#88C72E",
+  "--dx": "calc(var(--size) - var(--x))",
+  "--dy": "calc(var(--size) - var(--y))",
   ":hover": {
     opacity: 1,
     border: "calc(2px + .85vw) solid rgba(255, 255, 255, .5)",
-    transition: ".5s background-color ease,    .2s border ease",
+    transition: ".5s background-color ease, .2s border ease",
     borderRadius:
       "calc(var(--x) / var(--size) * 100%) calc(var(--dx) / var(--size) * 100%) calc(var(--dx) / var(--size) * 100%)  calc(var(--x) / var(--size) * 100%) / calc(var(--y) / var(--size) * 100%) calc(var(--y) / var(--size) * 100%) calc(var(--dy) / var(--size) * 100%) calc(var(--dy) / var(--size) * 100%)",
+    ":before": {
+      content: "''",
+      backgroundImage: "url(/awesome.svg)",
+      backgroundRepeat: "round",
+      position: "absolute",
+      top: "0px",
+      right: "0px",
+      bottom: "0px",
+      left: "0px",
+      opacity: "0.7",
+    },
+  },
+  ":before": {
+    content: "''",
+    backgroundImage: "url(/well_done.svg)",
+    backgroundRepeat: "round",
+    position: "absolute",
+    top: "0px",
+    right: "0px",
+    bottom: "0px",
+    left: "0px",
+    opacity: "0.7",
   },
 }));
 
@@ -37,4 +63,6 @@ export const MainPhrase = styled("p", {
   fontSize: "2rem",
   textAlign: "center",
   padding: "20px",
+  marginTop: "20px",
+  marginBottom: "20px",
 });
