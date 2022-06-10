001f9d1f944be9b34c51bc12d3ef688f19963bcc
changMin <Dlatlqjsgh7862!>
2022-06-10 21:39:25 +0900

    [Add] MyPage init

--- front/pages/myPage/index.tsx
+++ front/pages/myPage/index.tsx
@@ -1,7 +1,12 @@
 import { NextPage } from "next";
 
 const myPage: NextPage = () => {
-  return <div>MyPage</div>;
+  return (
+    <div>
+      <div>다른사람</div>
+      <div>내 단어장</div>
+    </div>
+  );
 };
 
 export default myPage;
