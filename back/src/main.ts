36b79e9907b689a371464cf8dc76924e3dcd4b54
Heyden <nhm6947@gmail.com>
2022-06-11 13:45:56 +0900

    [Del] main.ts 사용하지 않은 모듈 삭제

--- back/src/main.ts
+++ back/src/main.ts
@@ -1,5 +1,5 @@
 import { NestFactory } from "@nestjs/core";
-import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { SwaggerModule } from "@nestjs/swagger";
 import { BaseAPIDocumentation } from "./api/base.document";
 import { AppModule } from "./app.module";
 
