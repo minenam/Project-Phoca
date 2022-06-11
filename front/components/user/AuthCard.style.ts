e725682748aaf2e4dbfb233e7fc487d80f90c882
rnrn99 <giu0213@naver.com>
2022-06-11 11:22:35 +0900

    [Add] title 설정

--- front/components/user/AuthCard.style.ts
+++ front/components/user/AuthCard.style.ts
@@ -21,7 +21,26 @@ export const Card = styled("div", {
   boxShadow: "0 8px 32px 0 rgba( 31, 38, 135, 0.37 )",
   backdropFilter: "blur( 4px )",
   width: "800px",
-  height: "500px",
+  height: "600px",
   borderRadius: "50px",
   border: "1px solid rgba( 255, 255, 255, 0.18 )",
+});
+
+export const TitleContainer = styled("div", {
+  height: "20%",
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+});
+
+export const Logo = styled("img", {
+  height: "40%",
+  objectFit: "contain",
+  cursor: "pointer",
+});
+
+export const Title = styled("h1", {
+  fontSize: "2rem",
+  fontWeight: 900,
+  textAlign: "center",
 });
--- front/components/user/AuthCard.tsx
+++ front/components/user/AuthCard.tsx
@@ -1,11 +1,28 @@
+import Link from "next/link";
 import { useRouter } from "next/router";
-import { AuthCardContainer, Card } from "./AuthCard.style";
+import {
+  AuthCardContainer,
+  Card,
+  TitleContainer,
+  Logo,
+  Title,
+} from "./AuthCard.style";
+import LoginPage from "./login/LoginPage";
 
 function AuthCard() {
   const router = useRouter();
+  const isLoginPage: boolean = router.pathname === "/login";
   return (
-    <AuthCardContainer $login={router.pathname === "/login"}>
-      <Card></Card>
+    <AuthCardContainer $login={isLoginPage}>
+      <Card>
+        <TitleContainer>
+          <Link href="/">
+            <Logo src="/logo.png" alt="logo" />
+          </Link>
+          <Title>{isLoginPage ? "로그인" : "회원가입"}</Title>
+        </TitleContainer>
+        {isLoginPage && <LoginPage />}
+      </Card>
     </AuthCardContainer>
   );
 }
