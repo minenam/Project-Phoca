73ef4a7342b5618d16517ae54d628f0ffb53b842
changMin <Dlatlqjsgh7862!>
2022-06-13 21:06:04 +0900

    [Add] My Page Layout

--- front/pages/myPage/index.tsx
+++ front/pages/myPage/index.tsx
@@ -2,12 +2,20 @@ import { NextPage } from "next";
 import {
   Browser,
   MyPageWrapper,
-  UserInfo,
   UserInfoEdit,
   Avatar,
   AvatarImage,
   Wrapper,
   Triangle,
+  UserName,
+  EmailRoundedBox,
+  UserInfoDetail,
+  UserInfoWrapper,
+  UserDetailWrapper,
+  UserWrapper,
+  Branch,
+  ImgWrapper,
+  Seal,
 } from "./MyPage.style";
 
 const myPage: NextPage = () => {
@@ -16,12 +24,29 @@ const myPage: NextPage = () => {
       <Wrapper>
         <UserInfoEdit>회원 정보 수정하기</UserInfoEdit>
       </Wrapper>
-      <UserInfo>
-        <Avatar>
-          <AvatarImage src="/vercel.svg" alt="avatar" />
-        </Avatar>
-      </UserInfo>
-      <Wrapper>
+      <UserWrapper>
+        <UserInfoWrapper>
+          <Avatar>
+            <AvatarImage src="/vercel.svg" alt="avatar" />
+          </Avatar>
+          <UserName>OOO님</UserName>
+        </UserInfoWrapper>
+        <UserInfoWrapper>
+          <UserDetailWrapper>
+            <EmailRoundedBox>이메일</EmailRoundedBox>
+            <UserInfoDetail>test@test.com</UserInfoDetail>
+          </UserDetailWrapper>
+          <UserDetailWrapper>
+            <EmailRoundedBox>코멘트</EmailRoundedBox>
+            <UserInfoDetail>테스트 코멘트</UserInfoDetail>
+          </UserDetailWrapper>
+        </UserInfoWrapper>
+        <ImgWrapper>
+          <Seal src="/logo.png" alt="seal" />
+          <Branch src="/branch.png" alt="branch" />
+        </ImgWrapper>
+      </UserWrapper>
+      <UserWrapper $box>
         <Browser>
           단어장 {"\n"}둘러보기
           <Triangle />
@@ -31,7 +56,7 @@ const myPage: NextPage = () => {
           내 단어장{"\n"}바로가기
           <Triangle />
         </Browser>
-      </Wrapper>
+      </UserWrapper>
     </MyPageWrapper>
   );
 };
