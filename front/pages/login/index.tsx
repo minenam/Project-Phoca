760b81295b97ff9cc67746a676390813d347366f
rnrn99 <giu0213@naver.com>
2022-06-14 14:31:32 +0900

    [Fix] page 구조 변경(AuthCard 삭제)

--- front/pages/login/index.tsx
+++ front/pages/login/index.tsx
@@ -1,8 +1,28 @@
+import Link from "next/link";
 import type { NextPage } from "next";
-import AuthCard from "../../components/user/AuthCard";
+import {
+  AuthCardContainer,
+  Card,
+  TitleContainer,
+  Logo,
+  Title,
+} from "../../components/user/AuthCard.style";
+import LoginPage from "../../components/user/login/LoginPage";
 
-const LoginPage: NextPage = () => {
-  return <AuthCard />;
+const Login: NextPage = () => {
+  return (
+    <AuthCardContainer $login>
+      <Card>
+        <TitleContainer>
+          <Link href="/">
+            <Logo src="/logo.png" alt="logo" />
+          </Link>
+          <Title>로그인</Title>
+        </TitleContainer>
+        <LoginPage />
+      </Card>
+    </AuthCardContainer>
+  );
 };
 
-export default LoginPage;
+export default Login;
