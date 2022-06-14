7b0635aa0f7ffecc029e0c43bad4c65655287e22
Heyden <nhm6947@gmail.com>
2022-06-15 01:23:36 +0900

    [Fix] app service, controller 삭제

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,12 +1,7 @@
 import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
-import { UserController } from "./user/user.controller";
 import { UserModule } from "./user/user.module";
 
 @Module({
   imports: [UserModule],
-  controllers: [AppController, UserController],
-  providers: [AppService],
 })
 export class AppModule {}
