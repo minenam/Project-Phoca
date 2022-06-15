fc2473d0cabc203176896aa687d2ea2daba22802
ksw <tlsdnddl12@naver.com>
2022-06-15 18:02:49 +0900

    [Add] word service, controller CRUD 추가 (보완 예정)

--- back/src/word/word.controller.ts
+++ back/src/word/word.controller.ts
@@ -1,31 +1,60 @@
 import {
+  Body,
   Controller,
   Delete,
   Get,
   Param,
+  Patch,
   Post,
   UploadedFile,
   UseInterceptors,
 } from "@nestjs/common";
 import { FileInterceptor } from "@nestjs/platform-express";
+import { CreateWordDto } from "./dto/create-word.dto";
+import { UpdateWordDto } from "./dto/update-word.dto";
 import { WordService } from "./word.service";
 
 @Controller("word")
 export class WordController {
   constructor(private readonly wordService: WordService) {}
 
-  // @Get("/:wordbookId")
-  // getAll() {
-  //   return this.wordService.getAll();
-  // }
-  @Post()
-  @UseInterceptors(FileInterceptor("file"))
-  uploadImage(@UploadedFile() file: Express.Multer.File) {
-    return this.wordService.uploadImage(file);
+  @Get("all/:wordbookId")
+  getAll(@Param("wordbookId") wordbookId: string) {
+    return this.wordService.getAll(wordbookId);
   }
 
-  @Delete("/:key")
-  deleteImage(@Param("key") key: string) {
-    return this.wordService.deleteImage(key);
+  @Get("/:wordId")
+  get(@Param("wordId") wordId: string) {
+    return this.wordService.get(wordId);
+  }
+
+  @Post("/create/:wordbookId")
+  createWord(
+    @Param("wordbookId") wordbookId: string,
+    @Body("body") createWordDto: CreateWordDto,
+  ) {
+    return this.wordService.create(wordbookId, createWordDto);
+  }
+  @Post("/:wordbookId")
+  @UseInterceptors(FileInterceptor("file"))
+  uploadWord(
+    @UploadedFile() file: Express.Multer.File,
+    @Body() createWordDto: CreateWordDto,
+    @Param("wordbookId") wordbookId: string,
+  ) {
+    return this.wordService.uploadWord(file, createWordDto, wordbookId);
+  }
+
+  @Patch("/:wordId")
+  updateWord(
+    @Param("wordId") wordId: string,
+    @Body() updateWordDto: UpdateWordDto,
+  ) {
+    return this.wordService.update(wordId, updateWordDto);
+  }
+
+  @Delete("/:wordId")
+  deleteWord(@Param("wordId") wordId: string) {
+    return this.wordService.deleteWord(wordId);
   }
 }
--- back/src/word/word.service.ts
+++ back/src/word/word.service.ts
@@ -1,8 +1,10 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, NotFoundException } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import * as AWS from "aws-sdk";
 import * as dotenv from "dotenv";
 import { Repository } from "typeorm";
+import { CreateWordDto } from "./dto/create-word.dto";
+import { UpdateWordDto } from "./dto/update-word.dto";
 import { Word } from "./word.entity";
 dotenv.config();
 
@@ -19,7 +21,11 @@ export class WordService {
   ) {}
   s3 = new AWS.S3();
 
-  async uploadImage(file: Express.Multer.File) {
+  async uploadWord(
+    file: Express.Multer.File,
+    createWordDto: CreateWordDto,
+    wordbookId: string,
+  ) {
     const AWS_S3_BUCKET = process.env.AWS_BUCKET_NAME;
     const params = {
       Bucket: AWS_S3_BUCKET,
@@ -30,27 +36,68 @@ export class WordService {
     try {
       const response = await this.s3.upload(params).promise();
       console.log(response);
-      return response.Location;
+      const { wordEng, wordKor } = createWordDto;
+      const wordImage = response.Location;
+      const newWord = this.wordRepository.create({
+        wordEng,
+        wordKor,
+        wordImage,
+        // wordbookId,
+      });
+      return await this.wordRepository.save(newWord);
     } catch (e) {
       console.log(e);
     }
   }
 
-  async deleteImage(key: string) {
-    const response = await this.s3
-      .deleteObject({
-        Bucket: process.env.AWS_BUCKET_NAME,
-        Key: key,
-      })
-      .promise();
-    console.log(response);
-    return response;
+  async deleteWord(wordId: string) {
+    const word = await this.wordRepository.findOne({ where: { wordId } });
+    if (!word) {
+      throw new NotFoundException("word not found");
+    }
+    return this.wordRepository.remove(word);
+    //   const key = word.key
+    //   const response = await this.s3
+    //     .deleteObject({
+    //       Bucket: process.env.AWS_BUCKET_NAME,
+    //       Key: key,
+    //     })
+    //     .promise();
+    //   console.log(response);
+    //   return response;
+    // }
+    // catch(e) {
+    //   console.log(e);
+    // }
   }
-  catch(e) {
-    console.log(e);
+  async create(wordbookId, createWordDto: CreateWordDto) {
+    const data = { ...createWordDto, wordbookId };
+    const word = this.wordRepository.create(data);
+    return await this.wordRepository.save(word);
   }
 
-  // async getAll(wordbookId: string) {
+  async getAll(wordbookId) {
+    const wordbook = await this.wordRepository.find({
+      where: { wordbookId },
+    });
+    return wordbook;
+  }
+
+  async get(wordId: string) {
+    const word = await this.wordRepository.findOne({
+      where: { wordId },
+    });
+    return word;
+  }
+
+  async update(wordId: string, updateWordDto: UpdateWordDto) {
+    const word = await this.wordRepository.findOne({ where: { wordId } });
+    if (!word) {
+      throw new NotFoundException("word not found");
+    }
+    Object.assign(word, updateWordDto);
+    return this.wordRepository.save(word);
+  }
   //   return await this.wordRepository.find({ relations: { wordbook: true } });
   // }
 }
