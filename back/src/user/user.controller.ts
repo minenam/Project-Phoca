472a906df92004f2b8f554d1ee987c0b9f6fc652
Heyden <nhm6947@gmail.com>
2022-06-15 11:38:12 +0900

    [Add] User MVP CRUD (로컬)

--- back/src/user/user.controller.ts
+++ back/src/user/user.controller.ts
@@ -1,4 +1,56 @@
-import { Controller } from "@nestjs/common";
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  NotFoundException,
+  Param,
+  Patch,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from "@nestjs/common";
+import { User } from "./users.model";
+import { UserService } from "./user.service";
+import { CreateUserDto } from "./dto/create-user.dto";
 
 @Controller("user")
-export class UserController {}
+export class UserController {
+  constructor(private userService: UserService) {}
+
+  @Get()
+  test(): User[] {
+    return this.userService.getAll();
+  }
+
+  @Post()
+  @UsePipes(ValidationPipe)
+  register(@Body() createUserDto: CreateUserDto): User {
+    return this.userService.createUser(createUserDto);
+  }
+
+  @Get("/:userId")
+  login(@Param("userId") userId: string): User {
+    const user = this.userService.getUserById(userId);
+    if (!user) {
+      throw new NotFoundException("login is failed");
+    }
+    console.log(`login is successed`);
+    return user;
+  }
+
+  @Delete("/:userId")
+  withdraw(@Param("userId") userId: string): void {
+    this.userService.deleteUser(userId);
+    console.log(`withdraw : ${userId}`);
+  }
+
+  @Patch("/:userId")
+  updateUser(
+    @Param("userId") userId: string,
+    @Body() createUserDto: CreateUserDto,
+  ): User {
+    console.log(`update : ${createUserDto.userName}`);
+    return this.userService.updateUser(userId, createUserDto);
+  }
+}
--- back/src/user/user.service.ts
+++ back/src/user/user.service.ts
@@ -1,4 +1,53 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, NotFoundException } from "@nestjs/common";
+import { User } from "./users.model";
+import { v4 as uuid } from "uuid";
+import { CreateUserDto } from "./dto/create-user.dto";
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  private users: User[] = [];
+
+  getAll(): User[] {
+    return this.users;
+  }
+
+  createUser(createUserDto: CreateUserDto) {
+    const { userName, email, password } = createUserDto;
+    const user: User = {
+      userId: uuid(),
+      userName,
+      email,
+      password,
+      provider: "local",
+      joinedAt: new Date(),
+      lastloginedAt: new Date(),
+      activated: true,
+    };
+
+    this.users.push(user);
+    return user;
+  }
+
+  getUserById(userId: string): User {
+    const user = this.users.find((user) => user.userId === userId);
+
+    if (!user) {
+      throw new NotFoundException(`userId ${userId} 를 찾을 수 없습니다.`);
+    }
+    return user;
+  }
+
+  deleteUser(userId: string): void {
+    const getUser = this.getUserById(userId);
+    this.users = this.users.filter((user) => user.userId !== getUser.userId);
+  }
+
+  updateUser(userId: string, createUserDto: CreateUserDto) {
+    const { userName, email, password } = createUserDto;
+    const user = this.users.find((user) => user.userId === userId);
+    user.userName = userName;
+    user.email = email;
+    user.password = password;
+    return user;
+  }
+}
