f7488def31e6e9b60428562e8197dcd508a91870
Heyden <nhm6947@gmail.com>
2022-06-15 14:23:47 +0900

    [Add] 데이터베이스, TypeORM 기본 설정

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { typeORMconfig } from "./config/typeorm.config";
 import { UserModule } from "./user/user.module";
 
 @Module({
-  imports: [UserModule],
+  imports: [TypeOrmModule.forRoot(typeORMconfig), UserModule],
 })
 export class AppModule {}
