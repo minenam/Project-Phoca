3abde1dfb114a7b549d5b6bcc18c640260b605ac
rnrn99 <giu0213@naver.com>
2022-06-15 19:59:19 +0900

    [Add] EditModal initial setting

---
+++ front/components/word/results/EditForm.tsx
@@ -0,0 +1,5 @@
+function EditForm() {
+  return <div>EditForm</div>;
+}
+
+export default EditForm;
--- front/components/word/results/Results.tsx
+++ front/components/word/results/Results.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { useState } from "react";
 import {
   ResultsContainer,
   ImageContainer,
@@ -14,6 +15,8 @@ import {
   Button,
 } from "./Results.style";
 import { FaVolumeUp, FaEdit } from "react-icons/fa";
+import Modal from "../../../common/modal/Modal";
+import EditForm from "./EditForm";
 
 interface Word {
   word?: string;
@@ -24,6 +27,8 @@ function Results() {
   const router = useRouter();
   const { word, imageUrl }: Word = router.query;
 
+  const [open, setOpen] = useState(false);
+
   const ttsBtnClickHandler = () => {
     const temp: SpeechSynthesisUtterance = new SpeechSynthesisUtterance(word);
     const voice: SpeechSynthesisVoice[] = window.speechSynthesis.getVoices();
@@ -31,30 +36,43 @@ function Results() {
     window.speechSynthesis.speak(temp);
   };
 
+  const editBtnClickHandler = () => {
+    setOpen(true);
+  };
+
+  const modalCloseHandler = () => {
+    setOpen(false);
+  };
+
   return (
-    <ResultsContainer>
-      <ImageContainer>
-        <ThumbImage src={imageUrl} alt="submit-image" />
-      </ImageContainer>
-      <WordContainer>
-        <EngWord>{word}</EngWord>
-        <IconContainer>
-          <TtsBtn onClick={ttsBtnClickHandler}>
-            <FaVolumeUp />
-          </TtsBtn>
-          <EditBtn>
-            <FaEdit />
-          </EditBtn>
-        </IconContainer>
-        <KorWord>사과</KorWord>
-      </WordContainer>
-      <BtnContainer>
-        <Link href="/word/upload">
-          <Button>사진 다시 찍기</Button>
-        </Link>
-        <Button>단어장 저장하기</Button>
-      </BtnContainer>
-    </ResultsContainer>
+    <>
+      <ResultsContainer>
+        <ImageContainer>
+          <ThumbImage src={imageUrl} alt="submit-image" />
+        </ImageContainer>
+        <WordContainer>
+          <EngWord>{word}</EngWord>
+          <IconContainer>
+            <TtsBtn onClick={ttsBtnClickHandler}>
+              <FaVolumeUp />
+            </TtsBtn>
+            <EditBtn onClick={editBtnClickHandler}>
+              <FaEdit />
+            </EditBtn>
+          </IconContainer>
+          <KorWord>사과</KorWord>
+        </WordContainer>
+        <BtnContainer>
+          <Link href="/word/upload">
+            <Button>사진 다시 찍기</Button>
+          </Link>
+          <Button>단어장 저장하기</Button>
+        </BtnContainer>
+      </ResultsContainer>
+      <Modal open={open} width="300px" onClose={modalCloseHandler}>
+        <EditForm />
+      </Modal>
+    </>
   );
 }
 
