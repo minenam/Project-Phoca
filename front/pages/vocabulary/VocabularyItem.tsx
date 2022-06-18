3015f0a19a28651b8a8262f986dd7a720e16e72e
changMin <Dlatlqjsgh7862!>
2022-06-18 14:54:45 +0900

    [Add] useRouter

--- front/pages/vocabulary/VocabularyItem.tsx
+++ front/pages/vocabulary/VocabularyItem.tsx
@@ -1,4 +1,4 @@
-import { FC, useState } from "react";
+import { FC, useEffect, useState } from "react";
 import { useIsLapTop } from "../../common/utils/useIsLapTop";
 import {
   BtnWrapper,
@@ -10,10 +10,16 @@ import {
 import { MdPublic } from "react-icons/md";
 import { FaLock, FaShareSquare } from "react-icons/fa";
 import { MOCKUP_DATA } from "../../common/utils/constant";
+import { useRouter } from "next/router";
 
 const VocabularyItem: FC = () => {
   const [vocaList, setVocaList] = useState([]);
+  const router = useRouter();
   const isLapTop = useIsLapTop();
+
+  useEffect(() => {
+    //라우터 따라서 요청 보내는 거 바꾸기
+  }, []);
 
   return (
     <GridWrapper $lapTop={isLapTop}>
