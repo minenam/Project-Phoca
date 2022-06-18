178182fc3c9176a9652580537d97876c421a1253
Heyden <nhm6947@gmail.com>
2022-06-18 23:24:08 +0900

    [Fix] any 타입 코드 리팩토링

---
+++ back/src/user/dto/login-user.dto.ts
@@ -0,0 +1,10 @@
+export class LoginUserInfo {
+  userid: string;
+  username: string;
+  email: string;
+  provider: string;
+  joinedAt: Date;
+  lastloginedAt: Date;
+  activated: boolean;
+  token: string;
+}
