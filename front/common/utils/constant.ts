5f72727248862f7b3e0f8b2c417330e3b9a02118
Changm-L <chang6585@naver.com>
2022-06-19 09:18:30 +0900

    [Add] intro 수정

--- front/common/utils/constant.ts
+++ front/common/utils/constant.ts
@@ -2,6 +2,7 @@ interface Button {
   buttonName: string;
   buttonColor: string;
   link: string;
+  backgroundImage: string;
 }
 
 interface MockUP {
@@ -14,21 +15,25 @@ export const MAIN_BUTTON: Button[] = [
     buttonName: "단어장 만들기",
     buttonColor: "#AF93FF",
     link: "/makeWord",
+    backgroundImage: "/monster_artist.svg",
   },
   {
     buttonName: "단어장 보러가기",
     buttonColor: "#FEC70B",
     link: "/myPage",
+    backgroundImage: "/traveling.svg",
   },
   {
     buttonName: "그림퀴즈 하러가기",
     buttonColor: "#48CFC8",
     link: "/imageQuiz",
+    backgroundImage: "/playing_cards.svg",
   },
   {
-    buttonName: "단어퀴즈 하러가기",
+    buttonName: "단어퀴즈 보러가기",
     buttonColor: "#FE7394",
     link: "/wordQuiz",
+    backgroundImage: "/popularity.svg",
   },
 ];
 
