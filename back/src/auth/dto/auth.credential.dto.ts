52429dcb08e27dbceedc435da6917f9f6a65c07e
Heyden <nhm6947@gmail.com>
2022-06-20 23:49:50 +0900

    [Fix] 이메일 유효성 데코레이터 수정

--- back/src/auth/dto/auth.credential.dto.ts
+++ back/src/auth/dto/auth.credential.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { IsString, Matches, MaxLength, MinLength } from "class-validator";
+import { IsString, IsEmail, MaxLength, MinLength } from "class-validator";
 
 export class AuthCredentialDto {
   @ApiProperty({
@@ -10,9 +10,7 @@ export class AuthCredentialDto {
   @IsString()
   @MinLength(4)
   @MaxLength(40)
-  @Matches(/^([a-z0-9]+)@([a-z0-9]+).(\.[A-Za-z]{2,3})$/, {
-    message: "email(ID) only accepts english and number",
-  })
+  @IsEmail()
   email: string;
 
   @ApiProperty({
@@ -22,8 +20,6 @@ export class AuthCredentialDto {
   })
   @IsString()
   @MinLength(4)
-  @Matches(/^[a-zA-Z0-9!@#$%^&*]*$/, {
-    message: "password only accpets english, number and !@#$%^&*.",
-  })
+  @IsEmail()
   password: string;
 }
--- back/src/user/dto/create-user.dto.ts
+++ back/src/user/dto/create-user.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { IsString, Matches, MaxLength, MinLength } from "class-validator";
+import { IsEmail, IsString, MaxLength, MinLength } from "class-validator";
 
 export class CreateUserDto {
   @ApiProperty({
@@ -20,9 +20,7 @@ export class CreateUserDto {
   @IsString()
   @MinLength(4)
   @MaxLength(40)
-  @Matches(/^([a-z0-9]+)@([a-z0-9]+).(\.[A-Za-z]{2,3})$/, {
-    message: "email(ID) only accepts english and number",
-  })
+  @IsEmail()
   email: string;
 
   @ApiProperty({
@@ -32,8 +30,6 @@ export class CreateUserDto {
   })
   @IsString()
   @MinLength(4)
-  @Matches(/^[a-zA-Z0-9!@#$%^&*]*$/, {
-    message: "password only accpets english, number and !@#$%^&*.",
-  })
+  @IsEmail()
   password: string;
 }
