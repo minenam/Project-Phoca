553e15f4902662584d2510b0c9ccb7ecb50a32b9
Heyden <nhm6947@gmail.com>
2022-06-23 00:26:11 +0900

    [Add] 퀴즈 모듈, 컨트롤러, 서비스 생성

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
-// import { typeORMconfig } from "./config/typeorm.config";
 import { UserModule } from "./user/user.module";
 import { AuthModule } from "./auth/auth.module";
 import { configAsync } from "./orm.config";
@@ -8,6 +7,7 @@ import { ConfigModule } from "@nestjs/config";
 import { WordbookModule } from "./wordbook/wordbook.module";
 import { WordModule } from "./word/word.module";
 import { BookmarkModule } from "./bookmark/bookmark.module";
+import { QuizModule } from "./quiz/quiz.module";
 
 @Module({
   imports: [
@@ -16,12 +16,12 @@ import { BookmarkModule } from "./bookmark/bookmark.module";
       envFilePath: [".env"],
     }),
     TypeOrmModule.forRootAsync(configAsync),
-    // TypeOrmModule.forRoot(typeORMconfig),
     UserModule,
     AuthModule,
     WordbookModule,
     WordModule,
     BookmarkModule,
+    QuizModule,
   ],
   controllers: [],
   providers: [],
