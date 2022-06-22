68ca7819a28f4bffed15ed23a3b3b7c164812ec8
rnrn99 <giu0213@naver.com>
2022-06-22 23:54:01 +0900

    [Add] 단어장 추가 api 연동

--- front/components/word/results/SaveForm/SaveForm.tsx
+++ front/components/word/results/SaveForm/SaveForm.tsx
@@ -1,4 +1,6 @@
 import { useState, useEffect } from "react";
+import { useQuery } from "react-query";
+import { userStore } from "../../../../zustand/store";
 import {
   ListContainer,
   ItemContainer,
@@ -19,66 +21,73 @@ interface SaveFormProps {
   onClose: () => void;
 }
 
-export interface Vocabulary {
-  name: string;
-  private: boolean;
+interface Wordbook {
+  wordbookId: string;
+  wordbookName: string;
+  secured: boolean;
 }
 
-const fakeData: Vocabulary[] = [
-  {
-    name: "단어장 1",
-    private: false,
-  },
-  {
-    name: "단어장 2",
-    private: true,
-  },
-  {
-    name: "단어장 3",
-    private: false,
-  },
-  {
-    name: "단어장 4",
-    private: false,
-  },
-];
+// 해당 유저의 단어장 리스트를 가져옴
+const getWordbookList = async (userId: string | undefined) => {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_SERVER_URL}/wordbook/user/${userId}`,
+    {
+      headers: {
+        Authorization: `Bearer ${sessionStorage.getItem("userToken")}`,
+      },
+    },
+  );
+  const result = await res.json();
+  return result;
+};
 
 function SaveForm({ onClose }: SaveFormProps) {
-  const [vocabularies, setVocabularies] = useState<Vocabulary[]>(fakeData);
-  const [selectedVocabulary, setSelectedVocabulary] = useState<Vocabulary>({
-    name: "",
-    private: false,
+  const user = userStore((state) => state.user);
+  const { data } = useQuery("wordbookList", async () => {
+    return getWordbookList(user?.userId);
   });
-  const [addFormOpen, setAddFormOpen] = useState(false);
 
+  const [wordbookList, setWordbookList] = useState<Wordbook[]>([]); // 단어장 리스트를 저장
+  const [selectedWordbook, setSelectedWordbook] = useState<Wordbook>({
+    wordbookId: "",
+    wordbookName: "",
+    secured: false,
+  }); // 선택된 단어장을 저장
+  const [addFormOpen, setAddFormOpen] = useState(false); // 새 단어장 추가 폼의 열림 상태를 저장
+
+  // 기존 리스트에서 단어장을 선택할 수 있는 checkbox 핸들러
   const checkboxClickHandler = (e: React.FormEvent<HTMLInputElement>) => {
     const { value } = e.currentTarget;
-    const selected = vocabularies.filter((item, idx) => idx === Number(value));
+    const selected = wordbookList.filter((item, idx) => idx === Number(value));
     if (selected) {
-      setSelectedVocabulary(selected[0]);
+      setSelectedWordbook(selected[0]);
     }
   };
 
   useEffect(() => {
-    console.log(selectedVocabulary);
-  }, [selectedVocabulary]);
+    console.log(selectedWordbook);
+  }, [selectedWordbook]);
+
+  useEffect(() => {
+    setWordbookList(data);
+  }, [data]);
 
   return (
     <>
       <SelectBookContainer>
         <Title>단어장 저장</Title>
         <ListContainer $height="250px">
-          {vocabularies.map((item, idx) => (
-            <ItemContainer key={item.name}>
+          {wordbookList?.map((item, idx) => (
+            <ItemContainer key={item.wordbookName}>
               <input
                 type="checkbox"
                 name="word"
                 value={idx}
-                checked={item.name === selectedVocabulary.name}
+                checked={item.wordbookName === selectedWordbook.wordbookName}
                 onChange={checkboxClickHandler}
               />
-              <Label>{item.name}</Label>
-              <Label>{item.private ? <AiFillLock /> : <MdPublic />}</Label>
+              <Label>{item.wordbookName}</Label>
+              <Label>{item.secured ? <AiFillLock /> : <MdPublic />}</Label>
             </ItemContainer>
           ))}
         </ListContainer>
@@ -86,7 +95,7 @@ function SaveForm({ onClose }: SaveFormProps) {
       <AddBookBtn onClick={() => setAddFormOpen((cur) => !cur)}>
         <AiOutlinePlusCircle /> 새 단어장 추가
       </AddBookBtn>
-      {addFormOpen && <AddForm setVocabularies={setVocabularies} />}
+      {addFormOpen && <AddForm userId={user?.userId} />}
       <BtnContainer>
         <Button type="submit">저장하기</Button>
         <Button onClick={onClose}>취소하기</Button>
