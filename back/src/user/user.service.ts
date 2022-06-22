edc91f9c1274c620b001c2df590f293df42a15af
Heyden <nhm6947@gmail.com>
2022-06-22 23:57:21 +0900

    [Fix] 로그인 및 유저 조회 API 반환 타입 변경

--- back/src/user/user.service.ts
+++ back/src/user/user.service.ts
@@ -9,9 +9,9 @@ import { Repository } from "typeorm";
 import { InjectRepository } from "@nestjs/typeorm";
 import { AuthService } from "../auth/auth.service";
 import { AuthCredentialDto } from "../auth/dto/auth.credential.dto";
-import { LoginUserInfo } from "../user/dto/login-user.dto";
+import { LoginUserInfoType } from "../user/dto/login-user.dto";
 import { ImageMiddleware } from "../middleware/image.middleware";
-type LoginInfo = LoginUserInfo;
+import { UserInfoType } from "./dto/user-info.dto";
 
 @Injectable()
 export class UserService {
@@ -28,7 +28,7 @@ export class UserService {
   }
 
   // 유저 생성 (회원가입)
-  async register(createUserDto: CreateUserDto): Promise<string> {
+  async register(createUserDto: CreateUserDto): Promise<UserInfoType> {
     const { userName, email, password } = createUserDto;
     // 이메일 중복확인
     const foundEmail = await this.userRepository.findOneBy({ email });
@@ -42,13 +42,18 @@ export class UserService {
       email,
       password: hashedPassword,
     });
-    await this.userRepository.save(user);
+    const result = await this.userRepository.save(user);
+    const { password, provider, joinedAt, lastloginedAt, activated, ...userInfo } = result;
 
-    return `Welcome to Phoca, ${user.userName}`;
+    return {
+      statusCode: 201,
+      message: "success",
+      data: userInfo,
+      };
   }
 
   // 유저 로그인 (토큰 생성)
-  async login(authcredntialDto: AuthCredentialDto): Promise<LoginInfo> {
+  async login(authcredntialDto: AuthCredentialDto): Promise<LoginUserInfoType> {
     const { email } = authcredntialDto;
     const user = await this.userRepository.findOneBy({ email });
     // 마지막 로그인일자 업데이트
@@ -61,31 +66,37 @@ export class UserService {
   }
 
   // 유저 ID로 조회
-  async getUserById(userId: string): Promise<Users> {
+  async getUserById(userId: string): Promise<UserInfoType> {
     const getUser = await this.userRepository.findOneBy({ userId });
 
     if (!getUser) {
       throw new NotFoundException(`can't find userid ${userId}`);
     }
-    return getUser;
+    const { password, provider, joinedAt, lastloginedAt, activated, ...userInfo } = getUser;
+    return {
+      statusCode: 200,
+      message: "success",
+      data: userInfo,
+      };
   }
 
   // 유저 계정 삭제
   async deleteUser(userId: string): Promise<string> {
     const found = await this.getUserById(userId);
     if (found) {
-      await this.imageMiddleware.deleteImage(found.userImage);
+      await this.imageMiddleware.deleteImage(found.data.userImage);
     }
     const result = await this.userRepository.delete({ userId });
+    console.log("result: ", result);
     if (result.affected === 0) {
       throw new NotFoundException(`Can't find user`);
     }
-    return `Good Bye, User :${found.userName}`;
+    return `Good Bye, User :${found.data.userName}`;
   }
 
   // 유저 정보 (이름, 코멘트, 이미지) 수정
   async updateUser(userId: string, updateUserInfo) {
-    const user = await this.getUserById(userId);
+    const user = await this.userRepository.findOneBy({ userId });
     if (!user) {
       throw new NotFoundException(`can't find user`);
     }
