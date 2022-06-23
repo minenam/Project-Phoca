1e3f6518e229ab2a4b594372f471f51100969294
rnrn99 <giu0213@naver.com>
2022-06-23 19:31:58 +0900

    [Add] 단어 정보 가져오기

--- front/pages/word/results/[id].tsx
+++ front/pages/word/results/[id].tsx
@@ -1,17 +1,43 @@
 import type { NextPage } from "next";
+import { GetServerSideProps } from "next";
+import { useRouter } from "next/router";
+import { useQuery } from "react-query";
 import {
   WORD_NOTE_WIDTH,
   WORD_NOTE_HEIGHT,
 } from "../../../common/utils/constant";
 import Note from "../../../common/note/Note";
 import Results from "../../../components/word/results/Results";
+import { ResultProps, WordInfo } from "../../../common/types/resultsType";
 
-const ResultPage: NextPage = () => {
+const ResultPage: NextPage<ResultProps> = (props) => {
+  const router = useRouter();
+  const wordId = router.query.id;
+  const { data } = useQuery("wordInfo", () => getWord(wordId), {
+    initialData: props.wordInfo,
+  });
+
   return (
     <Note width={WORD_NOTE_WIDTH} height={WORD_NOTE_HEIGHT}>
-      <Results />
+      <Results wordInfo={data} />
     </Note>
   );
+};
+
+const getWord = async (wordId: string | string[] | undefined) => {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_SERVER_URL}/word/${wordId}`,
+  );
+  const result = await res.json();
+  return result;
+};
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const wordId = context.query.id;
+  const wordInfo: WordInfo = await getWord(wordId);
+  return {
+    props: { wordInfo },
+  };
 };
 
 export default ResultPage;
