a42ad59ec0f58800bb2ec052b98338e2534f1199
Heyden <nhm6947@gmail.com>
2022-06-24 23:07:50 +0900

    [Fix] 유저 API 결과 메시지 한글로 수정

--- back/src/user/user.service.ts
+++ back/src/user/user.service.ts
@@ -33,7 +33,9 @@ export class UserService {
     // 이메일 중복확인
     const foundEmail = await this.userRepository.findOneBy({ email });
     if (foundEmail) {
-      throw new ConflictException(`Already exist ${email}`);
+      throw new ConflictException(
+        `이미 존재하는 이메일입니다. 다시 입력해주세요.`,
+      );
     }
     // 새로운 유저 저장
     const hashedPassword = await this.authService.hashedUser(password);
@@ -76,7 +78,7 @@ export class UserService {
     const getUser = await this.userRepository.findOneBy({ userId });
 
     if (!getUser) {
-      throw new NotFoundException(`can't find userid ${userId}`);
+      throw new NotFoundException(`존재하지 않는 회원입니다.`);
     }
     const {
       password,
@@ -102,7 +104,7 @@ export class UserService {
     const result = await this.userRepository.delete({ userId });
     console.log("result: ", result);
     if (result.affected === 0) {
-      throw new NotFoundException(`Can't find user`);
+      throw new NotFoundException(`존재하지 않는 회원입니다.`);
     }
     return `Good Bye, User :${found.data.userName}`;
   }
@@ -111,7 +113,7 @@ export class UserService {
   async updateUser(userId: string, updateUserInfo) {
     const user = await this.userRepository.findOneBy({ userId });
     if (!user) {
-      throw new NotFoundException(`can't find user`);
+      throw new NotFoundException(`존재하지 않는 회원입니다.`);
     }
     const { userName, comment, file } = updateUserInfo;
     if (userName) {
