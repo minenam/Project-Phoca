00bcf914cdb1cd0f3a2481bb9f3ea835f32fc889
rnrn99 <giu0213@naver.com>
2022-06-24 13:53:40 +0900

    [Fix] next/link import 수정

--- front/components/sidebar/SideBar.tsx
+++ front/components/sidebar/SideBar.tsx
@@ -1,5 +1,4 @@
-import React from "react";
-import Link from "next/Link";
+import Link from "next/link";
 import { useStyletron } from "styletron-react";
 import { MAIN_BUTTON, HEADER_HEIGHT } from "../../common/utils/constant";
 import { SideBarContainer, SideBarBtn } from "./SideBar.style";
