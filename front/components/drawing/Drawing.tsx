1b06f2ccd6ac778ecbe5cde632f9718f2fff3261
rnrn99 <giu0213@naver.com>
2022-06-29 01:19:03 +0900

    [Add] 문제 연결

--- front/components/drawing/Drawing.tsx
+++ front/components/drawing/Drawing.tsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useQuery } from "react-query";
 import CanvasComp from "./Canvas";
 import {
   DrawingContainer,
@@ -10,11 +11,35 @@ import {
   ResetBtn,
 } from "./Drawing.style";
 
+interface DrawingWord {
+  id: number;
+  wordEng: string;
+  wordKor: string;
+}
+
+const getDrawingWord = async () => {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/quiz`);
+  const result: DrawingWord = await res.json();
+  return result;
+};
+
 function Drawing() {
   const [resetBtnClick, setResetBtnClick] = useState(false);
+  const [selectedWord, setSelectedWord] = useState<DrawingWord | undefined>(
+    undefined,
+  );
+
+  const { data } = useQuery("drawing-word", getDrawingWord, {
+    enabled: selectedWord === undefined,
+  });
+
+  useEffect(() => {
+    setSelectedWord(data);
+  }, [data]);
+
   return (
     <DrawingContainer>
-      <Question>Apple</Question>
+      <Question>{data && data.wordEng}</Question>
       <ResetBtnContainer>
         <ResetBtn onClick={() => setResetBtnClick(true)}>모두 지우기</ResetBtn>
       </ResetBtnContainer>
