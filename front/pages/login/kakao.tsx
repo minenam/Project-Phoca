8b072c14f28b00101c5bb602462e3d87a5719148
rnrn99 <giu0213@naver.com>
2022-06-30 13:35:20 +0900

    [Fix] login/kakao 페이지 수정

--- front/pages/login/kakao.tsx
+++ front/pages/login/kakao.tsx
@@ -1,38 +1,8 @@
 import { useRouter } from "next/router";
-import { useEffect } from "react";
-import { useMutation } from "react-query";
-
-const kakaoLoginHandler = async (code: string) => {
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_SERVER_URL}/auth/kakao/login`,
-    {
-      method: "POST",
-      body: JSON.stringify(code),
-    },
-  );
-  if (!res.ok) {
-    throw new Error("카카오 로그인 에러");
-  }
-  const result = await res.json();
-  return result;
-};
 
 function Kakao() {
   const router = useRouter();
-  const code = router.query.code as string;
-
-  const kakaoLoginMutation = useMutation(kakaoLoginHandler, {
-    onSuccess: (data, variables) => {
-      console.log("kakao login 성공: ", data);
-    },
-    onError: (err) => {
-      console.log(err);
-    },
-  });
-
-  useEffect(() => {
-    kakaoLoginMutation.mutate(code);
-  }, []);
+  console.log(router);
   return <div>Kakao</div>;
 }
 
